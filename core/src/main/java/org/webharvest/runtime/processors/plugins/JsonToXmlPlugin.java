29c8786c73880a3a570ac8877faa6707e85b3ff6
wajda79 <wajda79@a57e4815-81e3-4922-91ae-0d9c2f5eb974>
2010-12-07 23:56:57 +0000

    <json-to-xml> produces not well-formed XML. https://sourceforge.net/projects/web-harvest/forums/forum/591298/topic/4004217
    git-svn-id: svn://svn.code.sf.net/p/web-harvest/code/trunk@356 a57e4815-81e3-4922-91ae-0d9c2f5eb974

--- core/src/main/java/org/webharvest/runtime/processors/plugins/JsonToXmlPlugin.java
+++ core/src/main/java/org/webharvest/runtime/processors/plugins/JsonToXmlPlugin.java
@@ -15,15 +15,24 @@ import org.webharvest.runtime.variables.Variable;
  */
 public class JsonToXmlPlugin extends WebHarvestPlugin {
 
+    private final static String ATTR_ROOT_TAG_NAME = "tag";
+
+    @Override
     public String getName() {
         return "json-to-xml";
     }
 
+    @Override
+    public String[] getValidAttributes() {
+        return new String[]{ATTR_ROOT_TAG_NAME};
+    }
+
+    @Override
     public Variable executePlugin(Scraper scraper, ScraperContext context) throws InterruptedException {
-        Variable body = executeBody(scraper, context);
         try {
-            JSONObject jsonObject = new JSONObject(body.toString());
-            return new NodeVariable( XML.toString(jsonObject) );
+            return new NodeVariable(XML.toString(
+                    new JSONObject(executeBody(scraper, context).toString()),
+                    evaluateAttribute(ATTR_ROOT_TAG_NAME, scraper)));
         } catch (JSONException e) {
             throw new PluginException("Error converting JSON to XML: " + e.getMessage());
         }
